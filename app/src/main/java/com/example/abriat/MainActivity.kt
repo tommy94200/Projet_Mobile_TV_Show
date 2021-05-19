8bc8a6c8d4c398c0619598f920fa880354a3d707
tommy94200 <abriat@et.esiea.fr>
2021-05-19 19:22:28 +0200

    ajout barre de recherche dans main activity

--- app/src/main/java/com/example/abriat/MainActivity.kt
+++ app/src/main/java/com/example/abriat/MainActivity.kt
@@ -2,21 +2,39 @@
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-
+        //partie recherche
+        val search = findViewById<View>(R.id.searchbar) as EditText
+        search.visibility = View.INVISIBLE
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+          /*  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()*/
+            //affichage de la zone de texte et entrée de la variable de rercherche
+           // search.visibility = View.VISIBLE
+            when(search.visibility){
+                View.INVISIBLE  -> {
+                    search.visibility = View.VISIBLE
+                }
+                View.VISIBLE -> {
+                    search.visibility = View.INVISIBLE
+                    val request = search.text.toString()   //display the text that you entered in edit text
+                   // Snackbar.make(view, request, Snackbar.LENGTH_LONG).setAction("Action", null).show()
+                }
+            }
+
+
         }
     }
 
